5b5dcbd1c4dfa9032e51e4229c60ce0abed2321e
Renato Macedo <renato_macedo_1@hotmail.com>
2022-09-13 10:15:02 +0100

    display list

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import React, { useState, useEffect } from 'react'
-import { useFetch } from './useFetch'
-import Follower from './Follower'
+import React, { useState, useEffect } from "react";
+import { useFetch } from "./useFetch";
+import Follower from "./Follower";
 function App() {
-  return <h2>pagination starter</h2>
+  const { loading, data } = useFetch();
+
+  return (
+    <main>
+      <div className="section-title">
+        <h1>{loading ? "loading..." : "pagination"}</h1>
+        <p>{loading ? "" : "bitcoin's github followers"}</p>
+        <div className="underline"></div>
+      </div>
+      <section className="followers">
+        <div className="container">
+          {data.map((follower) => {
+            return <Follower key={follower.name} {...follower} />;
+          })}
+        </div>
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
